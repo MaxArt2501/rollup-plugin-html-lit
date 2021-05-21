bdfbbe303d4705adc0a15b999ac6b9c7afd10853
Massimo Artizzu <massimo.artizzu@antreem.com>
2021-05-21 09:18:39 +0200

    Add tests for 100% coverage

--- src/plugin.spec.ts
+++ src/plugin.spec.ts
@@ -1,3 +1,4 @@
+import { SourceDescription } from "rollup";
 import { html } from "./plugin";
 
 describe("Rollup plugin HTML/Lit", () => {
@@ -9,11 +10,51 @@ describe("Rollup plugin HTML/Lit", () => {
 
   describe("transform", () => {
     it("returns nothing if the source file isn't an .html file", () => {
-      // const ctx = jasmine.createSpyObj("TransformPluginContext", [
-      //   "addWatchFile",
-      // ]);
       const transform: any = html().transform;
       expect(transform("", "a.txt")).toBeUndefined();
+    });
+    it("watches a single file if a string is passed in the `watch` option", () => {
+      const ctx = { addWatchFile: jest.fn() };
+      const transform: any = html({ watch: "foo.html" }).transform;
+      transform.call(ctx, "", "/src/a.html");
+      expect(ctx.addWatchFile).toHaveBeenCalledWith("foo.html");
+    });
+    it("watches multiple files if an array of strings is passed in the `watch` option", () => {
+      const ctx = { addWatchFile: jest.fn() };
+      const transform: any = html({
+        watch: ["foo.html", "bar.html"],
+      }).transform;
+      transform.call(ctx, "", "/src/a.html");
+      expect(ctx.addWatchFile.mock.calls).toEqual([["foo.html"], ["bar.html"]]);
+    });
+    it("minifies the HTML if the option `minify` is `true`", () => {
+      const transform: any = html({
+        minify: true,
+      }).transform;
+      const result: SourceDescription = transform(
+        "<b>  ${test}  </b>",
+        "/src/a.html"
+      );
+      expect(result.code).toBe(`import { html } from 'lit-html';
+
+export default function() {
+  return html\`<b>\${test}</b>\`;
+}`);
+    });
+    it("prints the error if one has been thrown", () => {
+      jest.spyOn(console, "error");
+      const transform: any = html().transform;
+      transform("Incorrectly `escaped` ${backticks}", "/src/a.html");
+      expect((console.error as jest.Mock).mock.calls).toEqual([
+        ["Error:\n\tCannot read property 'flatMap' of undefined"],
+        ["Line:   undefined"],
+        ["Column: undefined"],
+      ]);
+      (console.error as jest.Mock).mockRestore();
+    });
+    it("rethrows the errore if the option `failOnError` is `true`", () => {
+      const transform: any = html({ failOnError: true }).transform;
+      expect(() => transform("Incorrectly `escaped` ${backticks}", "/src/a.html")).toThrow();
     });
   });
 });
